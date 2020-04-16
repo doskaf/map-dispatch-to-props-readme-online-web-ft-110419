c09084d93602abdf02e33d9874fbfa759a44b22b
Dominique Skaf <doskaf@yahoo.com>
2020-04-15 23:12:35 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+  handleOnClick = event => {
+    this.props.addItem()
   }
 
   render() {
@@ -20,11 +20,5 @@ class App extends Component {
     );
   }
 };
-
-const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
-};
-
-export default connect(mapStateToProps)(App);
+ 
+export default connect(state => ({ items: state.items }), { addItem })(App);
